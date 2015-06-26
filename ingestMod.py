3dd8e4e8e4c61fadd344f9af6eb5f9af87db1e58
Brian Carrier <carrier@sleuthkit.org>
2015-06-26 13:07:45 -0400

    Updated header

--- ingestMod.py
+++ ingestMod.py
@@ -1,31 +1,31 @@
-# Sample module in the public domain. Feel free to use this as a template
-# for your modules (and you can remove this header and take complete credit
-# and liability)
 #
-# Contact: Brian Carrier [carrier <at> sleuthkit [dot] org]
+# Autopsy module based on Mari DeGrazia's Google analytic parser.
+# This file contains her original code, plus the basics of an
+# Autopsy ingest module. The GA-Parser code should probably be
+# its own file.
 #
-# This is free and unencumbered software released into the public domain.
+###########################################################################
 #
-# Anyone is free to copy, modify, publish, use, compile, sell, or
-# distribute this software, either in source code form or as a compiled
-# binary, for any purpose, commercial or non-commercial, and by any
-# means.
+#GA-Parser
+#This program carves and parses Google Analytic vales
+#and outputs them into a tsv form.
 #
-# In jurisdictions that recognize copyright laws, the author or authors
-# of this software dedicate any and all copyright interest in the
-# software to the public domain. We make this dedication for the benefit
-# of the public at large and to the detriment of our heirs and
-# successors. We intend this dedication to be an overt act of
-# relinquishment in perpetuity of all present and future rights to this
-# software under copyright law.
+#To read more about it, visit my blog at http://az4n6.blogspot.com/
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-# OTHER DEALINGS IN THE SOFTWARE.
+# Copyright (C) 2014 Mari DeGrazia (arizona4n6@gmail.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You can view the GNU General Public License at <http://www.gnu.org/licenses/>
+
 
 import jarray
 import sys
@@ -98,7 +98,7 @@ class CookieModule(FileIngestModule):
         lcFileName = file.getName().lower()
         lcParentPath = file.getParentPath().lower()
         #if lcFileName.find("cookies") != -1 or lcFileName.find(".txt") != -1 :
-        if lcFileName.find("cookies") != -1 or lcParentPath.endswith("Windows\Cookies") != -1 and lcFileName.find(".txt") != -1 :
+        if lcFileName.find("cookies") != -1 or lcParentPath.endswith("windows\cookies") != -1 and lcFileName.find(".txt") != -1 :
             self.gFile = file
             #tempFileName = "C:\\temp\\mak_" + uuid.uuid4()
             #print "temp file : " + tempFileName
